c4f9821ebdb0778f609f3a4efd2c1910c1c9ce97
oku <yotubaki8171@gmail.com>
2023-06-29 12:05:53 +0000

    users_controller修正

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @restaurant = @user.restaurants
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_users_path
+  end
+
+  def unsubscribe
+    @user = User.find(params[:id])
+    @user.update(is_deleted: !@user.is_deleted)
+    @user.destroy_unsubscribe_user_info
+    flash[:notice] = "退会処理を実行しました"
+    redirect_to admin_users_path
+  end
+end
